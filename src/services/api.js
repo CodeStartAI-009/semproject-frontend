3625adcb88cd1c64c5354e3e3bc38cede7f8ccbb
varun devapathni <varundevapathni@varuns-MacBook-Air.local>
2026-04-03 10:30:23 +0530

    fixed useEffect dependency

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5001/api",
+  baseURL: "https://movie-3xlv.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
